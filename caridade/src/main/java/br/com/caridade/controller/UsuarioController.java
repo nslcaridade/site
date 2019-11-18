c5b9fa03a382af6f677f498150f842f35844646b
Alexandre Duarte <alexandre.duarte@services.cmsoftware.com.br>
2019-11-18 16:54:49 -0300

    Incluindo altualização de participantes

--- caridade/src/main/java/br/com/caridade/controller/UsuarioController.java
+++ caridade/src/main/java/br/com/caridade/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.caridade.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -84,4 +85,30 @@ public class UsuarioController {
 		
 		return mv;
 	}
+    
+    @GetMapping("/atualiza")
+	public ModelAndView preparaAtualizacao() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("usuario/atualiza");
+		List<PastoralCaridade> ltPast = pastoralCaridadeRepository.findAll();
+		mv.addObject("participante", ltPast);
+		mv.setStatus(HttpStatus.OK);
+		return mv;
+	}
+    
+    @PostMapping("/atualiza")
+   	public ModelAndView atualizacao(@RequestBody List<PastoralCaridade> ltPast) {
+   		ModelAndView mv = new ModelAndView();
+   		mv.setViewName("usuario/atualiza");
+   		for (PastoralCaridade pastoralCaridade : ltPast) {
+   			Optional<PastoralCaridade> findById =  pastoralCaridadeRepository.findById(pastoralCaridade.getIdUsuario());
+   			findById.get().setCelular(pastoralCaridade.getCelular());
+   			findById.get().setSemana(pastoralCaridade.getSemana());
+   			findById.get().setMissaAcolhida(pastoralCaridade.getMissaAcolhida());
+   			findById.get().setHorarioAcolhida(pastoralCaridade.getHorarioAcolhida());
+			pastoralCaridadeRepository.saveAndFlush(findById.get());
+		}
+   		mv.setStatus(HttpStatus.OK);
+   		return mv;
+   	}
 }
