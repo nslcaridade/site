cc3def7b51d0ce3674545b22112d1f35c37e15dc
Alexandre Duarte <alexandre.duarte@services.cmsoftware.com.br>
2020-01-03 12:07:28 -0300

    Correcao do calendário por ano

--- caridade/src/main/java/br/com/caridade/controller/CalendarioCaridadeController.java
+++ caridade/src/main/java/br/com/caridade/controller/CalendarioCaridadeController.java
@@ -2,11 +2,14 @@ package br.com.caridade.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,11 +27,11 @@ public class CalendarioCaridadeController {
 	@Autowired
 	CalendarioCaridadeRepository calendarioCaridadeRepository;
 	
-	@GetMapping("/listar")
-	public ModelAndView listar() {
+	@GetMapping("/listar/{ano}")
+	public ModelAndView listar(@PathVariable Long ano,HttpServletResponse response) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("calendario/listar");
-		List<CalendarioCaridade> findCal = calendarioCaridadeRepository.findAll();
+		List<CalendarioCaridade> findCal = calendarioCaridadeRepository.findByCalendaByAno(ano);
 		mv.addObject("calendario", findCal);
 		mv.setStatus(HttpStatus.OK);
 		return mv;
--- caridade/src/main/java/br/com/caridade/repository/CalendarioCaridadeRepository.java
+++ caridade/src/main/java/br/com/caridade/repository/CalendarioCaridadeRepository.java
@@ -1,9 +1,26 @@
 package br.com.caridade.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.caridade.model.CalendarioCaridade;
 
+
 public interface CalendarioCaridadeRepository  extends JpaRepository<CalendarioCaridade, Long> {
 
+	@Query(value = "SELECT ID_CALENDARIO_CARIDADE," + 
+			       " DESCRICAO," +
+			       " DATA_CALENDARIO," +
+			       " CONFIRMADO," +
+			       " ID_BAZAR," +
+			       " ID_BINGO," +
+			       " ID_PASSEIO," +
+			       " ID_FESTA," +
+			       " ID_VIDEO FROM db_nsl.calendario_caridade WHERE YEAR(DATA_CALENDARIO) = :ano ORDER BY DATA_CALENDARIO",nativeQuery = true)
+	@Qualifier
+	List<CalendarioCaridade> findByCalendaByAno(@Param("ano") Long ano);
 }
